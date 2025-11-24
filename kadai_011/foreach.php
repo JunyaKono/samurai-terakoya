89775681c88f8c26c98dac266e2711cd8bf85aa1
JunyaKono <junya1733@gmail.com>
2025-11-24 13:42:52 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,12 @@
+<?php
+// 連想配列を作成
+$vegetable = [
+    "名前" => "玉ねぎ",
+    "値段" => 200,
+    "産地" => "北海道"
+];
+
+// foreach文でキーと値を出力
+foreach ($vegetable as $key => $value) {
+    echo $key . "：" . $value . "\n";
+}
