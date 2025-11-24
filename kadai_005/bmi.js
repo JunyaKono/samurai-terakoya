6f084ad9a964677cd0f8cec8156982ecebda28aa
JunyaKono <junya1733@gmail.com>
2025-11-24 13:08:52 +0900

    Delete kadai_005 directory

--- kadai_005/bmi.js
+++
@@ -1,8 +0,0 @@
-// 体重(kg)と身長(m)を変数に代入
-let weight = 68;   // 体重(kg)
-let height = 1.7;  // 身長(m)
-
-// BMIを計算する式: 体重 ÷ (身長 × 身長)
-let bmi = weight / (height * height);
-
-console.log("BMI: " + bmi);
